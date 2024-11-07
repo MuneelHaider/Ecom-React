fa7b7fa085f290818d570ff05faabd4300dd8b36
MuneelHaider <muneelhaider@gmail.com>
2024-11-07 21:47:53 +0500

    Updated Products

--- ecom-react/src/App.js
+++ ecom-react/src/App.js
@@ -1,13 +1,15 @@
 import Nav from "./components/Nav/Nav";
 import Recommended from "./components/Recommended/Recommended";
+import Sidebar from "./components/Sidebar/Sidebar";
 import Products from "./Products/Products";
 
 function App() {
   return (
     <>
+      <Sidebar />
       <Nav />
-      <Products />
       <Recommended />
+      <Products />
     </>
   );
 }
--- ecom-react/src/Products/Products.js
+++ ecom-react/src/Products/Products.js
@@ -1,33 +1,31 @@
 import "./Products.css";
+import data from "../DB/data";
 import { IoBagCheck } from "react-icons/io5";
 
 function Products() {
   return (
-    <>
-      <section className="card-container">
-        <section className="card">
-          <img
-            src="https://m.media-amazon.com/images/I/61uw5RDxKQL._AC_UY625_.jpg"
-            alt="Shoe"
-            className="card-img"
-          />
+    <section className="card-container">
+      {data.map((product, index) => (
+        <section className="card" key={index}>
+          <img src={product.img} alt={product.title} className="card-img" />
           <div className="card-details">
-            <h3 className="card-tile">Shoe</h3>
+            <h3 className="card-title">{product.title}</h3>
             <section className="card-reviews">
-              <span className="total-reviews">4</span>
+              <span>{product.star}</span>
+              <span className="total-reviews">{product.reviews}</span>
             </section>
             <section className="card-price">
               <div className="price">
-                <del>3000 PKR</del> 2000 PKR
+                <del>{product.prevPrice}</del> {product.newPrice} PKR
               </div>
               <div className="bag">
-                <IoBagCheck className="bag-icon"/>
+                <IoBagCheck className="bag-icon" />
               </div>
             </section>
           </div>
         </section>
-      </section>
-    </>
+      ))}
+    </section>
   );
 }
 
