f6cf0d1277922be93479117d92b28583433c0816
MuneelHaider <muneelhaider@gmail.com>
2024-11-07 23:56:58 +0500

    Added Hovers.

--- ecom-react/src/Sidebar/Sidebar.js
+++ ecom-react/src/Sidebar/Sidebar.js
@@ -2,13 +2,15 @@ import Category from "./Category/Category";
 import Price from "./Price/Price";
 import Colors from "./Colors/Colors";
 import "./Sidebar.css";
+import { GiConverseShoe } from "react-icons/gi";
+
 
 const Sidebar = ({ handleChange }) => {
   return (
     <>
       <section className="sidebar">
         <div className="logo-container">
-          <h1>🛒</h1>
+          <h1><a href="#"><GiConverseShoe /></a></h1>
         </div>
         <Category handleChange={handleChange} />
         <Price handleChange={handleChange} />
