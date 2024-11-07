6e17de3a711c4205cfc2f36e3dc79a90ec36c7bd
MuneelHaider <muneelhaider@gmail.com>
2024-11-07 20:12:35 +0500

    Products Updated.

--- ecom-react/src/Products/Products.js
+++ ecom-react/src/Products/Products.js
@@ -1,9 +1,34 @@
-import './Products.css'
+import "./Products.css";
+import { IoBagCheck } from "react-icons/io5";
 
 function Products() {
   return (
-    <div>Products</div>
-  )
+    <>
+      <section className="card-container">
+        <section className="card">
+          <img
+            src="https://m.media-amazon.com/images/I/61uw5RDxKQL._AC_UY625_.jpg"
+            alt="Shoe"
+            className="card-img"
+          />
+          <div className="card-details">
+            <h3 className="card-tile">Shoe</h3>
+            <section className="card-reviews">
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>3000 PKR</del> 2000 PKR
+              </div>
+              <div className="bag">
+                <IoBagCheck className="bag-icon"/>
+              </div>
+            </section>
+          </div>
+        </section>
+      </section>
+    </>
+  );
 }
 
 export default Products;
