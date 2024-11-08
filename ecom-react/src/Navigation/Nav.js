6cf3819dfd47781d7dd35926a64c6f2e2e03a56f
MuneelHaider <muneelhaider@gmail.com>
2024-11-08 19:02:19 +0500

    Removed Warnings - Due to Empty href links

--- ecom-react/src/Navigation/Nav.js
+++ ecom-react/src/Navigation/Nav.js
@@ -1,5 +1,6 @@
 import { FiHeart } from "react-icons/fi";
 import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+import AppURL from "../context/AppContext";
 import "./Nav.css";
 
 const Nav = ({ handleInputChange, query }) => {
@@ -15,13 +16,13 @@ const Nav = ({ handleInputChange, query }) => {
         />
       </div>
       <div className="profile-container">
-        <a href="#">
+        <a href={AppURL}>
           <FiHeart className="nav-icons" />
         </a>
-        <a href="">
+        <a href={AppURL}>
           <AiOutlineShoppingCart className="nav-icons" />
         </a>
-        <a href="">
+        <a href={AppURL}>
           <AiOutlineUserAdd className="nav-icons" />
         </a>
       </div>
--- ecom-react/src/Sidebar/Sidebar.js
+++ ecom-react/src/Sidebar/Sidebar.js
@@ -3,14 +3,14 @@ import Price from "./Price/Price";
 import Colors from "./Colors/Colors";
 import "./Sidebar.css";
 import { GiConverseShoe } from "react-icons/gi";
-
+import AppURL from "../context/AppContext";
 
 const Sidebar = ({ handleChange }) => {
   return (
     <>
       <section className="sidebar">
         <div className="logo-container">
-          <h1><a href="#"><GiConverseShoe /></a></h1>
+          <h1><a href={AppURL}><GiConverseShoe /></a></h1>
         </div>
         <Category handleChange={handleChange} />
         <Price handleChange={handleChange} />
